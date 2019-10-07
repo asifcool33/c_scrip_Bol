ee6e1ce8ad928d73ad65f44437135a72de4b4f11
asifcool33 <40834744+asifcool33@users.noreply.github.com>
2019-10-07 22:47:35 +0530

    Add files via upload

---
+++ multi_thread_headers.c
@@ -0,0 +1,76 @@
+//multithread example
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<time.h>
+#include<pthread.h>
+#include<signal.h>
+
+
+#define NUM_THREADS 1000000
+
+void broke(int s) {
+	// do nothing
+	printf("I am broken here.........\n");
+
+}
+
+
+void *create_headers(void *threadid)
+{	
+	long tid;
+	tid = (long)threadid;
+	int number;
+	char generated_request[1000];
+	
+	srand(time(0));
+
+char *acceptall[] = {
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+		"Accept-Encoding: gzip, deflate\r\n",
+		"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
+		"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
+		"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
+		"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+		"Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
+		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
+		"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+		"Accept: text/html, application/xhtml+xml",
+		"Accept-Language: en-US,en;q=0.5\r\n",
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
+		"Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n"};
+while(1){
+number = rand()%15;
+//printf("%d",number);
+
+strcpy(generated_request, acceptall[number]);
+printf("==================================================\n");
+printf("%s\n\n",generated_request);
+printf("=================================================\n");
+
+}
+//pthread_exit(NULL);
+}
+
+int main()
+{
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	int i;
+	signal(SIGPIPE, &broke);
+	while(1){
+	for(i=0; i<NUM_THREADS; i++)
+	{
+		//printf("main(): creating threads %d",i);
+		rc = pthread_create(&threads[i], NULL, create_headers,(void *)i);
+		if(rc){
+			printf("error:unable to create threads %d\n",rc);
+			exit(-1);
+		}
+		usleep(300000);
+	}
+	pthread_exit(NULL);
+}
+}
--- socket_client_oct.c
+++ socket_client_oct.c
@@ -24,7 +24,7 @@ int SocketConnect(int hSocket)
         int ServerPort = 80;
         struct sockaddr_in remote={0};
 
-        remote.sin_addr.s_addr = inet_addr("46.165.219.208"); //Local Host
+        remote.sin_addr.s_addr = inet_addr("127.0.0.1"); //Local Host
         remote.sin_family = AF_INET;
         remote.sin_port = htons(ServerPort);
 
